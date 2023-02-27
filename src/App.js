9d75975e33783757fdf4d0c8e96f9bcb18e93871
Prince Gehlot <prince@shoptrade.co>
2023-02-27 16:14:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Flip-Tile-Puzzle</h2>
+    </div>
+  );
+}
+
+export default App;
